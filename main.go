d0bacd3091534e4322ddeeabe4ecbd2ee0c1df7c
Gassayping <Dev_Gassayping@proton.me>
2025-11-22 12:05:45 -0500

    Server now reads files in the srv directory

--- main.go
+++ main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		fmt.Fprint(res, "Hello")
-	})
-
+	serveDir := os.DirFS("./srv")
+	handler := func(res http.ResponseWriter, req *http.Request) {
+		path := req.PathValue("file")
+		if path == "" {
+			path = "index.html"
+		}
+		file, err := fs.ReadFile(serveDir, path)
+		if err != nil {
+			res.WriteHeader(400)
+			fmt.Fprint(res, err)
+			return
+		}
+		fmt.Fprint(res, string(file))
+	}
+	http.HandleFunc("GET /{file...}", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
